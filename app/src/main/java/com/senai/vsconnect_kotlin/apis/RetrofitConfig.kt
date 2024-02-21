b72ce981cd4077b8d45658d5061c7daf44f404a5
Carlos Augusto Da Silva Santos <code.carlosaugusto@gmail.com>
2024-02-20 21:28:58 -0300

    Ajustes para o app rodar no tablet

--- app/src/main/java/com/senai/vsconnect_kotlin/apis/RetrofitConfig.kt
+++ app/src/main/java/com/senai/vsconnect_kotlin/apis/RetrofitConfig.kt
@@ -5,7 +5,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 class RetrofitConfig {
     companion object{
-        fun obterInstanciaRetrofit(url: String = "http://172.16.52.20:8090/") : Retrofit{
+        fun obterInstanciaRetrofit(url: String = "http://172.16.52.197:8090/") : Retrofit{
             return Retrofit.Builder()
                 .baseUrl(url)
                 .addConverterFactory(GsonConverterFactory.create())
